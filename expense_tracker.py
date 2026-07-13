f73ae7062c97d5bc540cfd99b757500a229fffa1
sierra-luis <sire.lm7@gmail.com>
2026-07-13 15:31:45 -0600

    Build calculate_total()

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,16 +1,13 @@
 class ExpenseTracker:
     def __init__(self):
         self.expenses = []
-    
 
     def add_expense(self,description,amount):
         expense = Expense(description,amount)
         self.expenses.append(expense)    
 
-
     def print_expenses(self,expense):
         print(f"{expense}")
-
 
     def show_expenses(self):
         if len(self.expenses)==0:
@@ -21,3 +18,10 @@ class ExpenseTracker:
             print("---------------------------------------------")
             for expense in self.expenses:
                 expense.print_expenses()
+
+    def calculate_total(self):
+        total = 0
+        for expense in self.expenses:
+            total += expense.amount
+        print(f"The expense's total is ${total:.2f}")    
+            
