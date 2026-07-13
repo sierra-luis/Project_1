602396352e46647ad127142eaf04f174512c9864
sierra-luis <sire.lm7@gmail.com>
2026-07-13 15:43:59 -0600

    Refactor show_expenses()

--- expense_tracker.py
+++ expense_tracker.py
@@ -6,9 +6,6 @@ class ExpenseTracker:
         expense = Expense(description,amount)
         self.expenses.append(expense)    
 
-    def print_expenses(self,expense):
-        print(f"{expense}")
-
     def show_expenses(self):
         if len(self.expenses)==0:
             print("-------There are no registred expenses-------")   
@@ -17,11 +14,10 @@ class ExpenseTracker:
             print("[                 EXPENSES                  ]")
             print("---------------------------------------------")
             for expense in self.expenses:
-                expense.print_expenses()
+                print(f"Expense:{expense.description}  Amount: ${expense.amount:.2f}")
 
     def calculate_total(self):
         total = 0
         for expense in self.expenses:
             total += expense.amount
         print(f"The expense's total is ${total:.2f}")    
-            
